8a39b2a929d0b93fb8a1743d847fc355fabf5571
GiteeMonkey <112243321+GiteeMonkey@users.noreply.github.com>
2020-09-19 23:03:33 +0000

    added font

--- aws.sh
+++ aws.sh
@@ -3,4 +3,9 @@
 # ~/aws.sh
 
 # Removed user's cached credentials
-# This script might be run with .dots, which uses 
+# This script might be run with .dots, which uses elevated privileges
+sudo -K
+
+echo "------------------------------"
+echo "Setting up AWS."
+echo "This script re
