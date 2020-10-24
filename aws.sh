4093a26909aba8543775ed14eda9b1727c92db06
GiteeMonkey <112243321+GiteeMonkey@users.noreply.github.com>
2020-10-24 19:07:15 +0000

    Remove unnecessary parens

--- aws.sh
+++ aws.sh
@@ -76,4 +76,10 @@ pip install awscli
 # Install if we don't have it
 if test ! $(which brew); then
   echo "Installing homebrew..."
-  ruby -e "$(curl -fsSL https://raw.githubuserco
+  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+fi
+
+# Make sure we’re using the latest Homebrew.
+brew update
+
+brew insta
