2e3fee5d0dc3f83b4748d45b815a12a8ee8e50fb
GiteeMonkey <112243321+GiteeMonkey@users.noreply.github.com>
2020-10-24 16:28:20 +0000

    back to original

--- aws.sh
+++ aws.sh
@@ -73,4 +73,7 @@ pip install awscli
 ###############################################################################
 
 # Check for Homebrew,
-# Install if we don
+# Install if we don't have it
+if test ! $(which brew); then
+  echo "Installing homebrew..."
+  ruby -e "$(curl -fsSL https://raw.githubuserco
