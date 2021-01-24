f42e90eb24af81efbd06203a0a6139657664d04c
GiteeMonkey <112243321+GiteeMonkey@users.noreply.github.com>
2021-01-24 11:04:26 +0000

    Rename parameter.

--- brew.sh
+++ brew.sh
@@ -114,3 +114,61 @@ brew install imagemagick --with-webp
 brew install lua
 brew install lynx
 brew install p7zip
+brew install pigz
+brew install pv
+brew install rename
+brew install rhino
+brew install speedtest_cli
+brew install ssh-copy-id
+brew install tree
+brew install webkit2png
+brew install zopfli
+brew install pkg-config libffi
+brew install pandoc
+
+# Lxml and Libxslt
+brew install libxml2
+brew install libxslt
+brew link libxml2 --force
+brew link libxslt --force
+
+# Install Heroku
+brew install heroku/brew/heroku
+heroku update
+
+# Core casks
+brew cask install --appdir="/Applications" alfred
+brew cask install --appdir="~/Applications" iterm2
+brew cask install --appdir="~/Applications" java
+brew cask install --appdir="~/Applications" xquartz
+
+# Development tool casks
+brew cask install --appdir="/Applications" sublime-text
+brew cask install --appdir="/Applications" atom
+brew cask install --appdir="/Applications" virtualbox
+brew cask install --appdir="/Applications" vagrant
+brew cask install --appdir="/Applications" macdown
+
+# Misc casks
+brew cask install --appdir="/Applications" google-chrome
+brew cask install --appdir="/Applications" firefox
+brew cask install --appdir="/Applications" skype
+brew cask install --appdir="/Applications" slack
+brew cask install --appdir="/Applications" dropbox
+brew cask install --appdir="/Applications" evernote
+brew cask install --appdir="/Applications" 1password
+#brew cask install --appdir="/Applications" gimp
+#brew cask install --appdir="/Applications" inkscape
+
+#Remove comment to install LaTeX distribution MacTeX
+#brew cask install --appdir="/Applications" mactex
+
+# Install Docker, which requires virtualbox
+brew install docker
+brew install boot2docker
+
+# Install developer friendly quick look plugins; see https://github.com/sindresorhus/quick-look-plugins
+brew cask install qlcolorcode qlstephen qlmarkdown quicklook-json qlprettypatch quicklook-csv betterzip qlimagesize webpquicklook suspicious-package quicklookase qlvideo
+
+# Remove outdated versions from the cellar.
+brew cleanup
---
+++ datastores.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+# Install command-line tools using Homebrew.
+
