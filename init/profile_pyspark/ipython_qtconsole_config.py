8fbe4440de4b99d1de4cee45d1194dd7a57c2530
GiteeMonkey <112243321+GiteeMonkey@users.noreply.github.com>
2021-05-15 15:53:03 +0000

    updated todo.

--- init/profile_pyspark/ipython_qtconsole_config.py
+++ init/profile_pyspark/ipython_qtconsole_config.py
@@ -50,3 +50,119 @@ c = get_config()
 
 # path to a custom CSS stylesheet
 # c.IPythonQtConsoleApp.stylesheet = ''
+
+# set the heartbeat port [default: random]
+# c.IPythonQtConsoleApp.hb_port = 0
+
+# Whether to overwrite existing config files when copying
+# c.IPythonQtConsoleApp.overwrite = False
+
+# set the iopub (PUB) port [default: random]
+# c.IPythonQtConsoleApp.iopub_port = 0
+
+# The IPython profile to use.
+# c.IPythonQtConsoleApp.profile = u'default'
+
+# JSON file in which to store connection info [default: kernel-<pid>.json]
+# 
+# This file will contain the IP, ports, and authentication key needed to connect
+# clients to this kernel. By default, this file will be created in the security-
+# dir of the current profile, but can be specified by absolute path.
+# c.IPythonQtConsoleApp.connection_file = ''
+
+# Set to display confirmation dialog on exit. You can always use 'exit' or
+# 'quit', to force a direct exit without any confirmation.
+# c.IPythonQtConsoleApp.confirm_exit = True
+
+# The name of the IPython directory. This directory is used for logging
+# configuration (through profiles), history storage, etc. The default is usually
+# $HOME/.ipython. This options can also be specified through the environment
+# variable IPYTHONDIR.
+# c.IPythonQtConsoleApp.ipython_dir = u''
+
+# Whether to install the default config files into the profile dir. If a new
+# profile is being created, and IPython contains config files for that profile,
+# then they will be staged into the new directory.  Otherwise, default config
+# files will be automatically generated.
+# c.IPythonQtConsoleApp.copy_config_files = False
+
+# Connect to an already running kernel
+# c.IPythonQtConsoleApp.existing = ''
+
+# Use a plaintext widget instead of rich text (plain can't print/save).
+# c.IPythonQtConsoleApp.plain = False
+
+# Start the console window with the menu bar hidden.
+# c.IPythonQtConsoleApp.hide_menubar = False
+
+# The Logging format template
+# c.IPythonQtConsoleApp.log_format = '[%(name)s]%(highlevel)s %(message)s'
+
+# 
+# c.IPythonQtConsoleApp.transport = 'tcp'
+
+#------------------------------------------------------------------------------
+# IPythonWidget configuration
+#------------------------------------------------------------------------------
+
+# A FrontendWidget for an IPython kernel.
+
+# IPythonWidget will inherit config from: FrontendWidget, HistoryConsoleWidget,
+# ConsoleWidget
+
+# The type of completer to use. Valid values are:
+# 
+# 'plain'   : Show the available completion as a text list
+#             Below the editing area.
+# 'droplist': Show the completion in a drop down list navigable
+#             by the arrow keys, and from which you can select
+#             completion by pressing Return.
+# 'ncurses' : Show the completion as a text list which is navigable by
+#             `tab` and arrow keys.
+# c.IPythonWidget.gui_completion = 'ncurses'
+
+# Whether to process ANSI escape codes.
+# c.IPythonWidget.ansi_codes = True
+
+# A CSS stylesheet. The stylesheet can contain classes for:
+#     1. Qt: QPlainTextEdit, QFrame, QWidget, etc
+#     2. Pygments: .c, .k, .o, etc. (see PygmentsHighlighter)
+#     3. IPython: .error, .in-prompt, .out-prompt, etc
+# c.IPythonWidget.style_sheet = u''
+
+# The height of the console at start time in number of characters (will double
+# with `vsplit` paging)
+# c.IPythonWidget.height = 25
+
+# 
+# c.IPythonWidget.out_prompt = 'Out[<span class="out-prompt-number">%i</span>]: '
+
+# 
+# c.IPythonWidget.input_sep = '\n'
+
+# Whether to draw information calltips on open-parentheses.
+# c.IPythonWidget.enable_calltips = True
+
+# 
+# c.IPythonWidget.in_prompt = 'In [<span class="in-prompt-number">%i</span>]: '
+
+# The width of the console at start time in number of characters (will double
+# with `hsplit` paging)
+# c.IPythonWidget.width = 81
+
+# A command for invoking a system text editor. If the string contains a
+# {filename} format specifier, it will be used. Otherwise, the filename will be
+# appended to the end the command.
+# c.IPythonWidget.editor = ''
+
+# If not empty, use this Pygments style for syntax highlighting. Otherwise, the
+# style sheet is queried for Pygments style information.
+# c.IPythonWidget.syntax_style = u''
+
+# The font family to use for the console. On OSX this defaults to Monaco, on
+# Windows the default is Consolas with fallback of Courier, and on other
+# platforms the default is Monospace.
+# c.IPythonWidget.font_family = u''
+
+# The pygments lexer class to use.
+# c.IPythonWidget.lexer_class = <IPython.utils.traitlets.Undefined object at 0x10258ded0>
