c80fe7d474241d53ed1ad5f94f281ac9f2223d5d
GiteeMonkey <112243321+GiteeMonkey@users.noreply.github.com>
2021-05-22 22:18:11 +0000

    added css

--- init/profile_pyspark/startup/00-pyspark-setup.py
+++ init/profile_pyspark/startup/00-pyspark-setup.py
@@ -5,4 +5,7 @@ import sys
 
 # Note: Some Spark installations do not need the extra libexec path
 spark_home = os.environ.get('SPARK_HOME', None)
-sys.path.inse
+sys.path.insert(0, spark_home + "/libexec/python")
+
+# Add the py4j to the path.
+# You may need to change the version number to match your insta
