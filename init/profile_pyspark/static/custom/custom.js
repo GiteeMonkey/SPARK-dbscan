a84d0f3872a7c54e01daa6dcbb25d2fb802b537a
GiteeMonkey <112243321+GiteeMonkey@users.noreply.github.com>
2021-05-29 11:34:21 +0000

    Fix tags.

--- init/profile_pyspark/static/custom/custom.js
+++ init/profile_pyspark/static/custom/custom.js
@@ -1,4 +1,7 @@
 // leave at least 2 line with only a star on it below, or doc generation fails
 /**
  *
- 
+ *
+ * Placeholder for custom user javascript
+ * mainly to be overridden in profile/static/custom/custom.js
+ * This will 
