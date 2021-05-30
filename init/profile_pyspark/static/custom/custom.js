149cdf8e3704c2fdfa8c149bd39950f104aec2c8
GiteeMonkey <112243321+GiteeMonkey@users.noreply.github.com>
2021-05-30 11:45:11 +0000

    Prepare 0.2.1 release

--- init/profile_pyspark/static/custom/custom.js
+++ init/profile_pyspark/static/custom/custom.js
@@ -7,4 +7,7 @@
  * This will always be an empty file in IPython
  *
  * User could add any javascript in the `profile/static/custom/custom.js` file
- * (and sho
+ * (and should create it if it does not exist).
+ * It will be executed by the ipython notebook at load time.
+ *
+ * Same 
