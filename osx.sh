d31a76a3067b3f864839b99199ddb13c2a5bef51
GiteeMonkey <112243321+GiteeMonkey@users.noreply.github.com>
2021-06-16 06:08:17 +0000

    :heart:

--- osx.sh
+++ osx.sh
@@ -2,4 +2,8 @@
 
 # ~/osx.sh — Originally from https://mths.be/osx
 
-# Ask for the administrator password upfron
+# Ask for the administrator password upfront
+sudo -v
+
+# Keep-alive: update existing `sudo` time stamp until `osx.sh` has finished
+while true; do sudo -n true; sleep
