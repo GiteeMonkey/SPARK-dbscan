204959440acbf22942bd9eec544d147aebe005c9
GiteeMonkey <112243321+GiteeMonkey@users.noreply.github.com>
2021-06-16 07:13:48 +0000

    New stuff

--- osx.sh
+++ osx.sh
@@ -6,4 +6,6 @@
 sudo -v
 
 # Keep-alive: update existing `sudo` time stamp until `osx.sh` has finished
-while true; do sudo -n true; sleep
+while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
+
+###################################################################
