27392be93ba295573dca76ed2e7a5876960852b1
GiteeMonkey <112243321+GiteeMonkey@users.noreply.github.com>
2021-06-20 07:56:15 +0000

    fix #22

--- osx.sh
+++ osx.sh
@@ -16,4 +16,7 @@ while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 #sudo scutil --set ComputerName "0x6D746873"
 #sudo scutil --set HostName "0x6D746873"
 #sudo scutil --set LocalHostName "0x6D746873"
-#sudo defaults write /Library/Preferences/SystemConfiguration/com.apple.smb.server NetBIO
+#sudo defaults write /Library/Preferences/SystemConfiguration/com.apple.smb.server NetBIOSName -string "0x6D746873"
+
+# Set standby delay to 24 hours (default is 1 hour or 3600)
+#sudo pmset -a stan
