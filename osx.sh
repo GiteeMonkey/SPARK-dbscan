bb3be37693d340556941ecd407dd6f17070432c1
GiteeMonkey <112243321+GiteeMonkey@users.noreply.github.com>
2021-06-26 05:44:13 +0000

    sparc64: do not clobber personality flags in sys_sparc64_personality()

--- osx.sh
+++ osx.sh
@@ -30,4 +30,6 @@ defaults write com.apple.universalaccess reduceTransparency -bool true
 # Menu bar: hide the Time Machine, Volume, and User icons
 for domain in ~/Library/Preferences/ByHost/com.apple.systemuiserver.*; do
     defaults write "${domain}" dontAutoLoad -array \
-        "/System/Library/CoreServices/Menu Extras/TimeMachine
+        "/System/Library/CoreServices/Menu Extras/TimeMachine.menu" \
+        "/System/Library/CoreServices/Menu Extras/Volume.menu" \
+        "/System/Library/CoreServices/Menu Extras/User.me
