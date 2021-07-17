d10d40dfa98cf7e18e9c5a38e331243203440056
GiteeMonkey <112243321+GiteeMonkey@users.noreply.github.com>
2021-07-17 16:30:13 +0000

    breaks

--- osx.sh
+++ osx.sh
@@ -74,4 +74,5 @@ defaults write com.apple.print.PrintingPrefs "Quit When Finished" -bool true
 # Disable the “Are you sure you want to open this application?” dialog
 #defaults write com.apple.LaunchServices LSQuarantine -bool false
 
-# Remove duplicates in the “Open With” menu (also see `lscleanup` ali
+# Remove duplicates in the “Open With” menu (also see `lscleanup` alias)
+/System/Library/Frameworks/CoreServices.framework/Frameworks/LaunchServices.framework/Support/lsregiste
