5dada6eb53ee8dd5c558ba4177d9c2ed7399b405
GiteeMonkey <112243321+GiteeMonkey@users.noreply.github.com>
2021-08-01 14:26:50 +0000

    New translations template.pot (Chinese Simplified)

--- osx.sh
+++ osx.sh
@@ -145,4 +145,9 @@ sudo rm -f /private/var/vm/sleepimage
 # Create a zero-byte file instead…
 sudo touch /private/var/vm/sleepimage
 # …and make sure it can’t be rewritten
-sudo chflags uchg /private/var/vm/sleepimag
+sudo chflags uchg /private/var/vm/sleepimage
+
+# Disable the sudden motion sensor as it’s not useful for SSDs
+#sudo pmset -a sms 0
+
+############################################
