d9e8ccee3a9a239b56ed9a7bf2fa14095b207c2b
GiteeMonkey <112243321+GiteeMonkey@users.noreply.github.com>
2021-08-01 08:07:19 +0000

    reorg files

--- osx.sh
+++ osx.sh
@@ -143,4 +143,6 @@ sudo pmset -a hibernatemode 0
 # Remove the sleep image file to save disk space
 sudo rm -f /private/var/vm/sleepimage
 # Create a zero-byte file instead…
-s
+sudo touch /private/var/vm/sleepimage
+# …and make sure it can’t be rewritten
+sudo chflags uchg /private/var/vm/sleepimag
