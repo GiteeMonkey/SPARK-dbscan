f075f0180ca8305d30a275c6e51aa2395bade7dc
GiteeMonkey <112243321+GiteeMonkey@users.noreply.github.com>
2021-08-04 17:42:15 +0000

    wtf?

--- osx.sh
+++ osx.sh
@@ -157,4 +157,7 @@ sudo chflags uchg /private/var/vm/sleepimage
 # Trackpad: enable tap to click for this user and for the login screen
 defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad Clicking -bool true
 defaults -currentHost write NSGlobalDomain com.apple.mouse.tapBehavior -int 1
-defaults write NSGlobalDomain com.apple.mouse.
+defaults write NSGlobalDomain com.apple.mouse.tapBehavior -int 1
+
+# Trackpad: map bottom right corner to right-click
+defaults write com.apple.driver.AppleBlueto
