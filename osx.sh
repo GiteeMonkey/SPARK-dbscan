f084553e541f8e2ddc1be160558ad220c52768a0
GiteeMonkey <112243321+GiteeMonkey@users.noreply.github.com>
2021-08-10 17:43:04 +0000

    change style

--- osx.sh
+++ osx.sh
@@ -173,4 +173,7 @@ defaults write com.apple.BluetoothAudioAgent "Apple Bitpool Min (editable)" -int
 
 # Enable full keyboard access for all controls
 # (e.g. enable Tab in modal dialogs)
-defaults write NSGlobalDoma
+defaults write NSGlobalDomain AppleKeyboardUIMode -int 3
+
+# Use scroll gesture with the Ctrl (^) modifier key to zoom
+defaults write com.apple.u
