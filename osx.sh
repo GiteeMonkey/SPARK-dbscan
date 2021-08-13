cf73044b7ee7a777af487900d431e7f59062e5d3
GiteeMonkey <112243321+GiteeMonkey@users.noreply.github.com>
2021-08-13 21:19:36 +0000

    v0.11.116.17+187

--- osx.sh
+++ osx.sh
@@ -178,4 +178,7 @@ defaults write NSGlobalDomain AppleKeyboardUIMode -int 3
 # Use scroll gesture with the Ctrl (^) modifier key to zoom
 defaults write com.apple.universalaccess closeViewScrollWheelToggle -bool true
 defaults write com.apple.universalaccess HIDScrollZoomModifierMask -int 262144
-# 
+# Follow the keyboard focus while zoomed in
+defaults write com.apple.universalaccess closeViewZoomFollowsFocus -bool true
+
+# D
