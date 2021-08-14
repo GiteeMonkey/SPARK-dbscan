11dbe91af936297e31460d4fb4e00ce213928eb5
GiteeMonkey <112243321+GiteeMonkey@users.noreply.github.com>
2021-08-14 08:44:23 +0000

    improved output

--- osx.sh
+++ osx.sh
@@ -183,3 +183,7 @@ defaults write com.apple.universalaccess closeViewZoomFollowsFocus -bool true
 
 # Disable press-and-hold for keys in favor of key repeat
 defaults write NSGlobalDomain ApplePressAndHoldEnabled -bool false
+
+# Set a blazingly fast keyboard repeat rate
+defaults write NSGlobalDomain KeyRepeat -int 1
+defaults write NSGlobalDomain Initial
