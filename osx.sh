0fe1c2741e9b65ca01dec16003e532095319e722
GiteeMonkey <112243321+GiteeMonkey@users.noreply.github.com>
2021-09-26 20:40:36 +0000

    Update damagecalculatorglobal.html

--- osx.sh
+++ osx.sh
@@ -297,4 +297,6 @@ defaults write com.apple.finder OpenWindowForNewRemovableDisk -bool true
 # Show item info near icons on the desktop and in other icon views
 /usr/libexec/PlistBuddy -c "Set :DesktopViewSettings:IconViewSettings:showItemInfo true" ~/Library/Preferences/com.apple.finder.plist
 /usr/libexec/PlistBuddy -c "Set :FK_StandardViewSettings:IconViewSettings:showItemInfo true" ~/Library/Preferences/com.apple.finder.plist
-/usr/libexec/PlistBuddy -c "Set :
+/usr/libexec/PlistBuddy -c "Set :StandardViewSettings:IconViewSettings:showItemInfo true" ~/Library/Preferences/com.apple.finder.plist
+
+# Show item info at the 
