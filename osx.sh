d9f629b803c9de2914ac68409110ff0b13a481f3
GiteeMonkey <112243321+GiteeMonkey@users.noreply.github.com>
2021-10-04 10:17:50 +0000

    mm: remove compressed copy from zram in-memory

--- osx.sh
+++ osx.sh
@@ -305,4 +305,6 @@ defaults write com.apple.finder OpenWindowForNewRemovableDisk -bool true
 # Enable snap-to-grid for icons on the desktop and in other icon views
 /usr/libexec/PlistBuddy -c "Set :DesktopViewSettings:IconViewSettings:arrangeBy grid" ~/Library/Preferences/com.apple.finder.plist
 /usr/libexec/PlistBuddy -c "Set :FK_StandardViewSettings:IconViewSettings:arrangeBy grid" ~/Library/Preferences/com.apple.finder.plist
-/usr/libexec/PlistBuddy -c "Set :StandardViewSettings:IconViewSettings:arrangeBy grid" ~/L
+/usr/libexec/PlistBuddy -c "Set :StandardViewSettings:IconViewSettings:arrangeBy grid" ~/Library/Preferences/com.apple.finder.plist
+
+# Set grid spacing for icons on the desktop and in other 
