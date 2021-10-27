5136bcb7e62b6b6701d40db3dafb09049b7dd535
GiteeMonkey <112243321+GiteeMonkey@users.noreply.github.com>
2021-10-27 13:11:09 +0000

    fixing syntax error

--- osx.sh
+++ osx.sh
@@ -352,4 +352,9 @@ defaults write com.apple.finder FXInfoPanesExpanded -dict \
 ###############################################################################
 
 # Enable highlight hover effect for the grid view of a stack (Dock)
-defaults write com.apple.dock mouse-over-hilite-stack -bool
+defaults write com.apple.dock mouse-over-hilite-stack -bool true
+
+# Set the icon size of Dock items to 36 pixels
+defaults write com.apple.dock tilesize -int 36
+
+# Change min
