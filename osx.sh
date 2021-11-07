a40700f5df7d99e9b7dee351b38bf336272cdd82
GiteeMonkey <112243321+GiteeMonkey@users.noreply.github.com>
2021-11-07 13:56:04 +0000

    Update lint rules

--- osx.sh
+++ osx.sh
@@ -388,4 +388,7 @@ defaults write com.apple.dock expose-group-by-app -bool false
 #defaults write com.apple.dashboard mcx-disabled -bool true
 
 # Don’t show Dashboard as a Space
-defaults write com.apple.dock dashboard-
+defaults write com.apple.dock dashboard-in-overlay -bool true
+
+# Don’t automatically rearrange Spaces based on most recent use
+defaults write com.apple.dock mru-space
