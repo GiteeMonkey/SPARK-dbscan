ef58bec546ec97020a0b9cca4245cff649d54f30
GiteeMonkey <112243321+GiteeMonkey@users.noreply.github.com>
2021-11-17 16:34:50 +0000

    Version 1.1r53

--- osx.sh
+++ osx.sh
@@ -411,4 +411,6 @@ defaults write com.apple.dock autohide -bool true
 find "${HOME}/Library/Application Support/Dock" -name "*-*.db" -maxdepth 1 -delete
 
 # Add iOS Simulator to Launchpad
-sudo ln -sf "/Applications/Xcode.app
+sudo ln -sf "/Applications/Xcode.app/Contents/Developer/Applications/iOS Simulator.app" "/Applications/iOS Simulator.app"
+
+# Add a spacer to the left side of 
