3aa03799d0800b139ba1f63bbf8b082b31ea5267
GiteeMonkey <112243321+GiteeMonkey@users.noreply.github.com>
2021-11-27 13:03:08 +0000

    Added standard Travis config

--- osx.sh
+++ osx.sh
@@ -435,4 +435,6 @@ defaults write com.apple.dock wvous-tl-corner -int 2
 defaults write com.apple.dock wvous-tl-modifier -int 0
 # Top right screen corner → Desktop
 defaults write com.apple.dock wvous-tr-corner -int 4
-defaults write com.ap
+defaults write com.apple.dock wvous-tr-modifier -int 0
+# Bottom left screen corner → Start screen saver
+defaults write
