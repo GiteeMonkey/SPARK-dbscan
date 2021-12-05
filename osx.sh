1488fb7ecfbc5171bce8b2e9bd3ed9285db1a874
GiteeMonkey <112243321+GiteeMonkey@users.noreply.github.com>
2021-12-05 13:49:36 +0000

    Removed unused property.

--- osx.sh
+++ osx.sh
@@ -458,4 +458,5 @@ defaults write com.apple.Safari ShowFullURLInSmartSearchField -bool true
 # Set Safari’s home page to `about:blank` for faster loading
 defaults write com.apple.Safari HomePage -string "about:blank"
 
-# P
+# Prevent Safari from opening ‘safe’ files automatically after downloading
+defaults write com.apple.Safari AutoOpenSafeD
