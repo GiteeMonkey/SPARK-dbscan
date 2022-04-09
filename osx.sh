58752695e9d6e7099eb06beccc9eb9e44283b6b3
GiteeMonkey <112243321+GiteeMonkey@users.noreply.github.com>
2022-04-09 18:02:24 +0000

    Update to HEAD of submodules

--- osx.sh
+++ osx.sh
@@ -689,4 +689,6 @@ defaults write com.apple.iCal IncludeDebugMenu -bool true
 defaults write com.apple.TextEdit RichText -int 0
 # Open and save files as UTF-8 in TextEdit
 defaults write com.apple.TextEdit PlainTextEncoding -int 4
-defaults write com.apple
+defaults write com.apple.TextEdit PlainTextEncodingForWrite -int 4
+# Set tab width to 4 instead of the default 8
+defaults write com.apple.TextEdit "TabW
