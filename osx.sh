1fad1423df708f28254d425ef735e3e8846b3d14
GiteeMonkey <112243321+GiteeMonkey@users.noreply.github.com>
2022-04-13 22:34:50 +0000

    Bump version to 0.7.3

--- osx.sh
+++ osx.sh
@@ -701,4 +701,7 @@ defaults write com.apple.DiskUtility advanced-image-options -bool true
 # Mac App Store                                                               #
 ###############################################################################
 
-# Enable the WebKit D
+# Enable the WebKit Developer Tools in the Mac App Store
+defaults write com.apple.appstore WebKitDeveloperExtras -bool true
+
+# E
