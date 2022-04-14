23b57910c39fb9babbe39aa167de60f2347992b4
GiteeMonkey <112243321+GiteeMonkey@users.noreply.github.com>
2022-04-14 15:31:18 +0000

    New translations ja-JP.yml (Polish)

--- osx.sh
+++ osx.sh
@@ -704,4 +704,7 @@ defaults write com.apple.DiskUtility advanced-image-options -bool true
 # Enable the WebKit Developer Tools in the Mac App Store
 defaults write com.apple.appstore WebKitDeveloperExtras -bool true
 
-# E
+# Enable Debug Menu in the Mac App Store
+defaults write com.apple.appstore ShowDebugMenu -bool true
+
+########################
