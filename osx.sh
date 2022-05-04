168347c3e2c237e8c445e952ce91cfe9e4f42952
GiteeMonkey <112243321+GiteeMonkey@users.noreply.github.com>
2022-05-04 12:28:58 +0000

    add ssl

--- osx.sh
+++ osx.sh
@@ -733,4 +733,7 @@ defaults write com.google.Chrome AppleEnableSwipeNavigateWithScrolls -bool false
 defaults write com.google.Chrome.canary AppleEnableSwipeNavigateWithScrolls -bool false
 
 # Disable the all too sensitive backswipe on Magic Mouse
-defaults write com.google.Chrome AppleEnableMouseSwipeNavigateWithScroll
+defaults write com.google.Chrome AppleEnableMouseSwipeNavigateWithScrolls -bool false
+defaults write com.google.Chrome.canary AppleEnableMouseSwipeNavigateWithScrolls -bool false
+
+# 
