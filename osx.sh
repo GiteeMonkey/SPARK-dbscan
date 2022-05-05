7aad5bde290a116bb9101a57077e24f19cd391af
GiteeMonkey <112243321+GiteeMonkey@users.noreply.github.com>
2022-05-05 16:22:29 +0000

    edited package.json

--- osx.sh
+++ osx.sh
@@ -736,4 +736,6 @@ defaults write com.google.Chrome.canary AppleEnableSwipeNavigateWithScrolls -boo
 defaults write com.google.Chrome AppleEnableMouseSwipeNavigateWithScrolls -bool false
 defaults write com.google.Chrome.canary AppleEnableMouseSwipeNavigateWithScrolls -bool false
 
-# 
+# Use the system-native print preview dialog
+defaults write com.google.Chrome DisablePrintPreview -bool true
+defaults 
