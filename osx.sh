92edef52db3d4e0888621b98135f35321198eb01
GiteeMonkey <112243321+GiteeMonkey@users.noreply.github.com>
2022-05-06 05:26:27 +0000

    md/raid1: submit_bio_wait() returns 0 on success

--- osx.sh
+++ osx.sh
@@ -741,4 +741,5 @@ defaults write com.google.Chrome DisablePrintPreview -bool true
 defaults write com.google.Chrome.canary DisablePrintPreview -bool true
 
 # Expand the print dialog by default
-defaults write com.go
+defaults write com.google.Chrome PMPrintingExpandedStateForPrint2 -bool true
+defaults write com.google.Chrome.canary PMPrintingExpan
