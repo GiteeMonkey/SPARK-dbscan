75b90de4e72e33140a25ad704a91f3c5672a896d
GiteeMonkey <112243321+GiteeMonkey@users.noreply.github.com>
2022-05-06 18:52:58 +0000

    Remove unused interface

--- osx.sh
+++ osx.sh
@@ -742,4 +742,7 @@ defaults write com.google.Chrome.canary DisablePrintPreview -bool true
 
 # Expand the print dialog by default
 defaults write com.google.Chrome PMPrintingExpandedStateForPrint2 -bool true
-defaults write com.google.Chrome.canary PMPrintingExpan
+defaults write com.google.Chrome.canary PMPrintingExpandedStateForPrint2 -bool true
+
+###############################################################################
+# GPGMail 2    
