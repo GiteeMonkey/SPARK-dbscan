f2ff4b50bced397a28ea604cfd27aab5b459496f
GiteeMonkey <112243321+GiteeMonkey@users.noreply.github.com>
2023-01-18 10:25:33 +0000

    Improve build process

--- osxprep.sh
+++ osxprep.sh
@@ -8,4 +8,7 @@ while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
 # Step 1: Update the OS and Install Xcode Tools
 echo "------------------------------"
-echo "Updating OSX.  If this requires a restart, run the script aga
+echo "Updating OSX.  If this requires a restart, run the script again."
+# Install all available updates
+sudo softwareupdate -ia --verbose
+# Install only recommended available up
