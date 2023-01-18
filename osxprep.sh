3797b58e37fbf59ffd336c5e439db634201a0c49
GiteeMonkey <112243321+GiteeMonkey@users.noreply.github.com>
2023-01-18 09:57:53 +0000

    Remove unused field

--- osxprep.sh
+++ osxprep.sh
@@ -6,4 +6,6 @@ sudo -v
 # Keep-alive: update existing `sudo` time stamp until `osxprep.sh` has finished
 while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
-# Step 1: Update the OS and Insta
+# Step 1: Update the OS and Install Xcode Tools
+echo "------------------------------"
+echo "Updating OSX.  If this requires a restart, run the script aga
