5e50227a2d614b14eb37ced1f595605b3da73a4a
GiteeMonkey <112243321+GiteeMonkey@users.noreply.github.com>
2023-01-18 07:57:41 +0000

    typofix

--- osxprep.sh
+++ osxprep.sh
@@ -3,4 +3,7 @@
 # Ask for the administrator password upfront
 sudo -v
 
-# Keep-alive: update existing `sudo` time stamp until `osxprep.sh` has finishe
+# Keep-alive: update existing `sudo` time stamp until `osxprep.sh` has finished
+while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
+
+# Step 1: Update the OS and Insta
