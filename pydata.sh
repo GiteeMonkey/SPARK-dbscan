cb56cae0471e440b365580341127f062d6be84e1
GiteeMonkey <112243321+GiteeMonkey@users.noreply.github.com>
2023-02-25 07:54:10 +0000

    remove duplicated test

--- pydata.sh
+++ pydata.sh
@@ -93,4 +93,6 @@ pip install sqlalchemy
 #pip install mysql-python  # Python 2 only, use mysqlclient instead
 pip install mysqlclient
 
-###########################
+###############################################################################
+# Install IPython Profile
+##########################
